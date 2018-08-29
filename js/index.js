91686c11ff6520d4558316dd9868f648aa2f68ad
Jeevanjot <jeevanjotsingh@yandex.com>
2018-08-29 09:47:04 +0530

    #3 part
    Show more button added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
   $('.tabs').tabs();
   showView(0);
+
+  $('.btn-floating').on("click", function() {
+    steem.api.getDiscussionsByTrending({
+        "tag": "steemit",
+        "limit": 10,
+        "start_permlink": window.permlink,
+        "start_author": window.author
+      },
+      function(err, result) {
+        setValues(err, result, 1);
+      });
+  });
 });
 //Running steem method on document ready when script tag initialized.
 //There is not a need for document.ready funcion but you can use them for practice.
@@ -11,7 +23,18 @@ function showView(val) {
         "tag": "steemit",
         "limit": 10
       }, function(err, result) {
-        setValues(err, result);
+        setValues(err, result, 0);
+      });
+      $('.btn-floating').on("click", function() {
+        steem.api.getDiscussionsByTrending({
+            "tag": "steemit",
+            "limit": 10,
+            "start_permlink": window.permlink,
+            "start_author": window.author
+          },
+          function(err, result) {
+            setValues(err, result, 1);
+          });
       });
       break;
 
@@ -20,7 +43,18 @@ function showView(val) {
         "tag": "steemit",
         "limit": 10
       }, function(err, result) {
-        setValues(err, result);
+        setValues(err, result, 0);
+      });
+      $('.btn-floating').on("click", function() {
+        steem.api.getDiscussionsByCreated({
+            "tag": "steemit",
+            "limit": 10,
+            "start_permlink": window.permlink,
+            "start_author": window.author
+          },
+          function(err, result) {
+            setValues(err, result, 1);
+          });
       });
       break;
 
@@ -29,7 +63,18 @@ function showView(val) {
         "tag": "steemit",
         "limit": 10
       }, function(err, result) {
-        setValues(err, result);
+        setValues(err, result, 0);
+      });
+      $('.btn-floating').on("click", function() {
+        steem.api.getDiscussionsByHot({
+            "tag": "steemit",
+            "limit": 10,
+            "start_permlink": window.permlink,
+            "start_author": window.author
+          },
+          function(err, result) {
+            setValues(err, result, 1);
+          });
       });
       break;
 
@@ -38,15 +83,28 @@ function showView(val) {
         "tag": "steemit",
         "limit": 10
       }, function(err, result) {
-        setValues(err, result);
+        setValues(err, result, 0);
+      });
+      $('.btn-floating').on("click", function() {
+        steem.api.getDiscussionsByPromoted({
+            "tag": "steemit",
+            "limit": 10,
+            "start_permlink": window.permlink,
+            "start_author": window.author
+          },
+          function(err, result) {
+            setValues(err, result, 1);
+          });
       });
       break;
   }
 }
-
-function setValues(err, result) {
-  $('.collection').empty();
-  for (i = 0; i < result.length; i++) {
+function setValues(err, result, post) {
+  // vm is used to get either the function is called from showing first posts or next...
+  if (post == 0) {
+    $('.collection').empty();
+  }
+  for (i = 0; i < result.length-1; i++) {
     //Looking for image, if existed then use the first of the post and if not then the use defined image.
     let imageUrl = JSON.parse(result[i].json_metadata);
     let image = "https://48tools.com/wp-content/uploads/2015/09/shortlink.png";
@@ -60,7 +118,7 @@ function setValues(err, result) {
     }
     // Replace if any html tag occur in post
     let bodyContent = result[i].body;
-    let prefined = bodyContent.replace(/(<([^>]+)>)/ig, "");
+    let prefined = bodyContent.replace(/(<([^>]+)>)/g, "");
     //Append <ul> tag body with proper steem post image, title and description.
     $(".collection").append("<li class='collection-item avatar'> \
     <a href='http://steemit.com" + result[i].url + "' style='color: inherit;'> \
@@ -71,4 +129,6 @@ function setValues(err, result) {
     </br>\
   </li>");
   }
+  window.permlink = result[i].permlink;
+  window.author = result[i].author;
 }
