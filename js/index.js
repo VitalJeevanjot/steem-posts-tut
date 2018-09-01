188616fa88c4f8d3c581f66ec10c87c5dc65a06c
Genievot <jeevanjotsingh@yandex.com>
2018-09-01 10:13:36 +0530

    #4 tutorail
    Filter posts with trending tags.
    Show beautiful navbar and side panel.

--- js/index.js
+++ js/index.js
@@ -1,33 +1,46 @@
+var tag = "";
 $(document).ready(function() {
   $('.tabs').tabs();
   showView(0);
+  $('.sidenav').sidenav();
 
-  $('.btn-floating').on("click", function() {
-    steem.api.getDiscussionsByTrending({
-        "tag": "steemit",
-        "limit": 10,
-        "start_permlink": window.permlink,
-        "start_author": window.author
-      },
-      function(err, result) {
-        setValues(err, result, 1);
-      });
+  steem.api.getTrendingTags("", 100, function(err, result) {
+    for (let i = 0; i < result.length; i++) {
+      if (result[i].name == "") {
+        $('.sidenav').append("<li><a class='waves-effect' href='#!' onclick=tagSet(\"" + result[i].name + "\")>All Tags</a></li>");
+      } else {
+        $('.sidenav').append("<li><a class='waves-effect' href='#!' onclick=tagSet(\"" + result[i].name + "\")>" + result[i].name + "</a></li>");
+      }
+    }
   });
 });
+
+function tagSet(ct) {
+  tag = ct;
+  $('.collection').empty();
+  console.log("Deldsdsdeted");
+  console.log(tag);
+  window.location = $('#tab11').attr('class'); //To refocus tabs over first tab
+  showView(0);
+}
 //Running steem method on document ready when script tag initialized.
 //There is not a need for document.ready funcion but you can use them for practice.
 function showView(val) {
   switch (val) {
     case 0:
       steem.api.getDiscussionsByTrending({
-        "tag": "steemit",
+        "tag": tag,
         "limit": 10
       }, function(err, result) {
+        console.log("trending");
         setValues(err, result, 0);
       });
-      $('.btn-floating').on("click", function() {
+      $("#btn-a").unbind();
+      $("#btn-a").bind("click", function() { // Otherwise there will be if/else statement...
+        //If we have to optimize it a little more.
+        console.log("trending clicked");
         steem.api.getDiscussionsByTrending({
-            "tag": "steemit",
+            "tag": tag,
             "limit": 10,
             "start_permlink": window.permlink,
             "start_author": window.author
@@ -40,14 +53,17 @@ function showView(val) {
 
     case 1:
       steem.api.getDiscussionsByCreated({
-        "tag": "steemit",
+        "tag": tag,
         "limit": 10
       }, function(err, result) {
+        console.log("c");
         setValues(err, result, 0);
       });
-      $('.btn-floating').on("click", function() {
+      $("#btn-a").unbind();
+      $("#btn-a").bind("click", function() {
+        console.log("c clicked");
         steem.api.getDiscussionsByCreated({
-            "tag": "steemit",
+            "tag": tag,
             "limit": 10,
             "start_permlink": window.permlink,
             "start_author": window.author
@@ -60,14 +76,16 @@ function showView(val) {
 
     case 2:
       steem.api.getDiscussionsByHot({
-        "tag": "steemit",
+        "tag": tag,
         "limit": 10
       }, function(err, result) {
         setValues(err, result, 0);
       });
-      $('.btn-floating').on("click", function() {
+      $("#btn-a").unbind();
+      $("#btn-a").bind("click", function() {
+        console.log("h clicked");
         steem.api.getDiscussionsByHot({
-            "tag": "steemit",
+            "tag": tag,
             "limit": 10,
             "start_permlink": window.permlink,
             "start_author": window.author
@@ -80,14 +98,16 @@ function showView(val) {
 
     case 3:
       steem.api.getDiscussionsByPromoted({
-        "tag": "steemit",
+        "tag": tag,
         "limit": 10
       }, function(err, result) {
         setValues(err, result, 0);
       });
-      $('.btn-floating').on("click", function() {
+      $("#btn-a").unbind();
+      $("#btn-a").bind("click", function() {
+        console.log("p clicked");
         steem.api.getDiscussionsByPromoted({
-            "tag": "steemit",
+            "tag": tag,
             "limit": 10,
             "start_permlink": window.permlink,
             "start_author": window.author
@@ -99,12 +119,13 @@ function showView(val) {
       break;
   }
 }
+
 function setValues(err, result, post) {
   // vm is used to get either the function is called from showing first posts or next...
   if (post == 0) {
     $('.collection').empty();
   }
-  for (i = 0; i < result.length-1; i++) {
+  for (i = 0; i < result.length - 1; i++) {
     //Looking for image, if existed then use the first of the post and if not then the use defined image.
     let imageUrl = JSON.parse(result[i].json_metadata);
     let image = "https://48tools.com/wp-content/uploads/2015/09/shortlink.png";
